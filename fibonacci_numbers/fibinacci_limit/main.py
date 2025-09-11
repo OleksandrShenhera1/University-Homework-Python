e83d48388c18aed444790fce65f7228e1a3e0f7b
Oleksandr <oleksandrshengra@gmail.com>
2025-09-11 09:16:48 +0300

    added: fibonacci limit, updated: reading functions.

--- fibonacci_numbers/fibinacci_limit/main.py
+++ fibonacci_numbers/fibinacci_limit/main.py
@@ -8,19 +8,18 @@ def read_file(file):
     print(f"Parsed nums: num_1 = {arr[0]}, num_2 = {arr[1]}, limit = {arr[-1]}.")
     return arr
 
-
-def recursive_fibonacci(arr, steps):
+def recursive_fibonacci(arr, limit):
     fibonacci_array = [float(x) for x in arr]
-    def recursive(arr, steps):
-        if steps == 0:
+    def recursive(arr, limit):
+        if arr[-2] + arr[-1] > limit:
             return
 
         next = arr[-2] + arr[-1]
         arr.append(next)
 
-        recursive(fibonacci_array, steps - 1)
+        recursive(fibonacci_array, limit)
 
-    recursive(fibonacci_array, steps)
+    recursive(fibonacci_array, limit)
 
     return fibonacci_array
 
@@ -31,8 +30,9 @@ def write_file(file, arr):
 if __name__ == '__main__':
     arr = read_file("nums.txt")
     if arr != None:
-        steps = int(arr[-1])
+        limit = float(arr[-1])
         arr.remove(arr[-1])
-        fibonacci_array = recursive_fibonacci(arr, steps)
+        fibonacci_array = recursive_fibonacci(arr, limit)
         write_file("../result.txt", fibonacci_array)
         print(f"Result: {fibonacci_array}")
+
